b096787aa11b75c6ee788302111367a14b4bd1db
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-12-09 10:36:14 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import '@controllers/UsersController'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' })
+})
+
+app.listen(3333)
